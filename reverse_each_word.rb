19a125030609107f40355b0413671d026ca0e630
Nicky Liu <nickyliu91@gmail.com>
2018-08-13 06:04:32 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(string)
   array = string.split
-  array.each do |word|
-    word.reverse!
+  array.collect do |word|
+    newarray = word.reverse
   end
-  array.join(' ')
 end
